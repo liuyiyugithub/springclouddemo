e35f4b40df2a7a44f67deb1e6498bbc8f087a6c8
liuyiyu <liuyiyua1@163.com>
2022-01-11 12:27:54 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,22 +12,5 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			while (true) {
-				int filever;
-				filever = Utilities.getControlFileVersion();
-				if (filever > Utilities.fileversion) {
-					Utilities.downloadData();
-					Utilities.fileversion = filever;
-				}
-				Thread.sleep(10000);
-			}
-
-		};
-	}
+	
 }
